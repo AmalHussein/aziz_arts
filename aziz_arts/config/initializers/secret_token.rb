720dcd11f6602e9032becb7dadcf49120e99ac89
Amal Hussein <amalhussein08@gmail.com>
2013-12-04 14:57:56 -0500

    initial commit setup rails app

---
+++ aziz_arts/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AzizArts::Application.config.secret_key_base = '1d5865f09aaed89ee6886c7763987c90c95f78a08a420ff3747209cacaeda5bfb414b2f388e182b290bc8dc4f6258dc5224259445442913aae697156f254abe8'
