c812952e9a6a32f69f8f71decadb29c7840e458e
Amal Hussein <amalhussein08@gmail.com>
2013-12-05 08:28:28 -0500

    setup line_items controller methods

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,2 +1,19 @@
 class LineItemsController < ApplicationController
+
+	def create 
+		product = Product.find(params[:product_id])
+		line_item = product.line_items.build(user: current_user)
+		line_item.save
+		redirect_to :cart 
+	end 
+
+	def destroy
+		LineItem.find(params[:id]).destroy
+		redirect_to :cart
+	end 
+
+	def index 
+		@line_items = current_user.line_items.in_cart
+	end 
+
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
 	belongs_to :product 
 	belongs_to :transaction 
 	belongs_to :user
+
+	scope :in_cart, -> {where order_id: nil}
+  scope :checked_out, -> {where "order_id IS NOT NULL" }
+  
 end
