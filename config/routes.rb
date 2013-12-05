fe5c08d617ad4ad6f407003bc8fab24e5f3c2544
Amal Hussein <amalhussein08@gmail.com>
2013-12-05 08:35:11 -0500

    added custom line_items routes to the config.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ AzizArts::Application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only: [:index, :show]
+ 	get '/cart' to: 'line_items#index', as: 'cart'
+ 	post '/products/:product_id/add_to_cart', to: 'line_items#create', as: 'add_product_to_cart'
+ 	delete '/cart/:id' to: 'line_items#destroy', as: 'remove_product_from_cart'
 
 end
