a73e79cb1fd3aa7c9239e6d44ea262fc7394d070
Amal Hussein <amalhussein08@gmail.com>
2013-12-08 22:49:27 -0500

    created cart scaffold, and store index. also ran migration

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,9 +1,10 @@
 class LineItem < ActiveRecord::Base
 	belongs_to :product 
 	belongs_to :transaction 
-	belongs_to :user
+	#belongs_to :user
+	belongs_to :cart 
 
-	scope :in_cart, -> {where transaction_id: nil}
-  scope :checked_out, -> {where "transaction_id IS NOT NULL" }
+	# scope :in_cart, -> {where transaction_id: nil}
+ #  scope :checked_out, -> {where "transaction_id IS NOT NULL" }
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 AzizArts::Application.routes.draw do
+  get "store/index"
+  resources :carts
+
   devise_for :users
   root 'products#index'
   resources :products, only: [:index, :show]
   resources :transactions, only: [:new , :create]
- 	get '/cart', to: 'line_items#index', as: 'cart'
+ 	#get '/cart', to: 'line_items#index', as: 'cart'
  	post '/products/:product_id/add_to_cart', to: 'line_items#create', as: 'add_product_to_cart'
- 	delete '/cart/:id', to: 'line_items#destroy', as: 'remove_product_from_cart'
+ 	#delete '/cart/:id', to: 'line_items#destroy', as: 'remove_product_from_cart'
 
 end
