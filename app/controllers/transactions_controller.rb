2c9fd1d8946036f8e024db118a060424a14325b7
Amal Hussein <amalhussein08@gmail.com>
2013-12-08 19:54:22 -0500

    added code to line items view for the cart

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,33 @@
 class TransactionsController < ApplicationController
 
-	
+	def new
+	end
 
+	def create
+	  # Amount in cents
+	  @amount = current_user.line_items.in_cart.sum {|item| item.product.price * 100 }.to_i
 
-	
+	  customer = Stripe::Customer.create(
+	    email: current_user.email,
+	    card: params[:stripeToken]
+	  )
+
+	  stripe_transaction = Stripe::Charge.create(
+	    customer: customer.id,
+	    amount: @amount,
+	    description: 'Rails Stripe customer',
+	    currency: 'usd'
+	  )
+
+	transaction = Transaction.create(total_price: @amount)
+	current_user.line_items.in_cart.each do |cart_item|
+		cart_item.transaction = transaction
+		cart_item.save
+	end
+
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to orders_path
+	end
+
 end
